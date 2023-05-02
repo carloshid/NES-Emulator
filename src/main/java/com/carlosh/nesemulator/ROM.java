53d1e748f04937bd3fe542e021a2db4947217e3a
carloshhid <carlos.hidalgomontesino@mail.mcgill.ca>
2023-05-02 04:24:43 -0400

    updated ROM class to read a the contents of a rom file

--- src/main/java/com/carlosh/nesemulator/ROM.java
+++ src/main/java/com/carlosh/nesemulator/ROM.java
@@ -2,9 +2,13 @@ package com.carlosh.nesemulator;
 
 import com.carlosh.nesemulator.mappers.Mapper;
 import com.carlosh.nesemulator.mappers.Mapper000;
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.List;
 
 public class ROM {
+  private int[] romBytes;
+  private String filename;
 
   private List<Integer> prgROM;
   private List<Integer> chrROM;
@@ -15,6 +19,9 @@ public class ROM {
   private int prgBanks = 0;
   private int chrBanks = 0;
 
+  private boolean valid = false;
+  private Mirror mirror = Mirror.HORIZONTAL;
+
   private class ROM_Header {
     String name;
     int prgROMSize;
@@ -23,14 +30,35 @@ public class ROM {
     int mapper2;
     int prgRAMSize;
     int tvSystem1;
+    int tvSystem2;
     String unused;
   }
 
-  public ROM(String name) {
-    this.header = new ROM_Header();
+  private enum Mirror {
+    HORIZONTAL,
+    VERTICAL,
+    ONESCREEN_LO,
+    ONESCREEN_HI
+  }
 
-    // TODO : Open file
-    // TODO :Read file header
+  public ROM(String filename) {
+    this.header = new ROM_Header();
+    valid = false;
+    romBytes = readFile(filename);
+    this.filename = filename;
+
+    header = loadHeader(romBytes);
+
+    if ((header.mapper1 & 0x04) != 0) {
+      // TODO: Skip trainer
+    }
+
+    // Mapper id and mirror
+    mapperID = ((header.mapper2 >> 4) << 4) | (header.mapper1 >> 4);
+    mirror = (header.mapper1 & 0x01) != 0 ? Mirror.VERTICAL : Mirror.HORIZONTAL;
+
+    // File type
+    int fileType = (header.mapper2 & 0x0C) == 0x08 ? 2 : 1;
 
     // Load mapper
     switch (mapperID) {
@@ -38,9 +66,10 @@ public class ROM {
         mapper = new Mapper000(prgBanks, chrBanks);
         break;
       }
+      // TODO: Add more mappers
     }
 
-    // TODO : close file
+    valid = true;
   }
 
   public boolean cpuRead(int address, boolean readOnly) {
@@ -77,5 +106,31 @@ public class ROM {
       return true;
     }
     return false;
+  }
+
+  private static int[] readFile(String filename) {
+    File file = new File(filename);
+    byte[] bytes = new byte[(int) file.length()];
+    FileInputStream stream;
+    try {
+      stream = new FileInputStream(file);
+      int res = stream.read(bytes);
+      stream.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    int[] romBytes = new int[bytes.length];
+    for (int i = 0; i < bytes.length; i++) {
+      romBytes[i] = bytes[i] & 0xFF;
+    }
+
+    return romBytes;
+  }
+
+  // TODO
+  private ROM_Header loadHeader(int[] romBytes) {
+    // TODO
+    return null;
   }
 }
